928b6678c5fcdeff36118057f45d9bebf9e1eeea
JulianBritos <julianbritos7@gmail.com>
2024-09-30 23:22:00 -0300

    New styles to form page and task page

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -1,21 +1,22 @@
 import { useEffect, useState } from "react";
 import { getAllTasks } from "../api/tasks.api";
-import {TaskCard} from "./TaskCard";
-
+import { TaskCard } from "./TaskCard";
 
 export function TaskList() {
-    const [tasks, setTasks] = useState([]);
-    
-    useEffect(() => {
-     async function loadTasks() {
-        const res = await getAllTasks();
-        setTasks(res.data);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    async function loadTasks() {
+      const res = await getAllTasks();
+      setTasks(res.data);
     }
-    loadTasks();},
-     [])
-    return(
-    <div>
-        {tasks.map((task) => (
-           <TaskCard key={task.id} task={task}/>
-        ))}
-    </div>)};
+    loadTasks();
+  }, []);
+  return (
+    <div className="grid grid-cols-3 gap-3">
+      {tasks.map((task) => (
+        <TaskCard key={task.id} task={task} />
+      ))}
+    </div>
+  );
+}
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { createTask, deleteTask, updateTask, getTask } from "../api/tasks.api";
 import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 export function TaskFormPage() {
   const {
@@ -16,8 +17,22 @@ export function TaskFormPage() {
   const onSubmit = handleSubmit(async (data) => {
     if (params.id) {
       await updateTask(params.id, data);
+      toast.success("Tarea Actualizada", {
+        position: "bottom-right",
+        style: {
+          background: "#101010",
+          color: "#fff",
+        },
+      });
     } else {
       await createTask(data);
+      toast.success("Tarea creada", {
+        position: "bottom-right",
+        style: {
+          background: "#101010",
+          color: "#fff",
+        },
+      });
     }
     navigate("/tasks");
   });
@@ -36,36 +51,73 @@ export function TaskFormPage() {
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="title"
-          {...register("title", { required: true })}
-        />
-        {errors.title && <span>Title is required</span>}
+        <div class="space-y-12">
+          <div class="border-b border-white-900/10 pb-12">
+            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div class="sm:col-span-4">
+                <label
+                  for="username"
+                  class="block text-sm font-medium leading-6 text-white-900"
+                >
+                  Task title
+                </label>
+                <div class="mt-2">
+                  <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-white-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                    <input
+                      class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-white-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                      type="text"
+                      placeholder="Write your task here"
+                      {...register("title", { required: true })}
+                    />
+                    {errors.title && <span>Title is required</span>}
+                  </div>
+                </div>
+              </div>
+            </div>
 
-        <textarea
-          rows="3"
-          placeholder="description"
-          {...register("description", { required: true })}
-        />
-        {errors.description && <span>Description is required</span>}
+            <div class="col-span-full">
+              <label
+                for="about"
+                class="block mt-4 text-sm font-medium leading-6 text-white-900"
+              >
+                Description
+              </label>
+              <div class="mt-2">
+                <textarea
+                  class="block pl-1 w-full rounded-md border-0 bg-transparent py-1.5 text-gray-100 shadow-sm ring-1 ring-inset focus-within:ring-indigo-600 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 "
+                  rows="3"
+                  placeholder="Description"
+                  {...register("description", { required: true })}
+                />
+                {errors.description && <span>Description is required</span>}
+              </div>
+              <div className="grid grid-cols-4">
+                <button className="mt-4 mr-10 bg-indigo-500 px-3 pl-2 rounded-lg ">
+                  Save
+                </button>
 
-        <button>Save</button>
+                {params.id && (
+                  <button
+                    className="mt-4 bg-red-500 px-3 pl-2 rounded-lg"
+                    onClick={async () => {
+                      const accepted = window.confirm("are you sure?");
+                      if (accepted) {
+                        await deleteTask(params.id);
+                        toast.success("Tarea Eliminada", {
+                          position: "bottom-right",
+                        });
+                        navigate("/tasks/");
+                      }
+                    }}
+                  >
+                    Delete
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
       </form>
-
-      {params.id && (
-        <button
-          onClick={async () => {
-            const accepted = window.confirm("are you sure?");
-            if (accepted) {
-              await deleteTask(params.id);
-              navigate("/tasks/");
-            }
-          }}
-        >
-          Delete
-        </button>
-      )}
     </div>
   );
 }

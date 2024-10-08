53e9f82dd07f5f5df05bd750682de81a774042e8
JulianBritos <julianbritos7@gmail.com>
2024-10-07 23:24:28 -0300

    requirement.txt and serializer.py added

--- django_crud_api/urls.py
+++ django_crud_api/urls.py
@@ -20,9 +20,15 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from tasks.views import CreateUserView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', include('tasks.urls'))
+    path('tasks/', include('tasks.urls')),
+    path("api/user/register/", CreateUserView.as_view(), name="regfister"),
+    path ("api/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path ("api/token/refresh", TokenRefreshView.as_view(), name="refresh"),
+    path ("api-auth/", include("rest_framework.urls")),
+    
 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-from .serializer import TaskSerializer
+from rest_framework import (viewsets, generics)
+from .serializer import (TaskSerializer, UserSerializer)
 from .models import Task
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
 
 
 # Create your views here.
 class TaskView(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = User
+    permission_classes = [AllowAny]    

8dd39a7cae4966126f780bac790a507bf3b5d52b
JulianBritos <julianbritos7@gmail.com>
2025-04-17 23:24:43 -0300

    error de merge arregaldo v2

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -31,6 +31,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/adminDashboard" element={<UserManagementPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Route>
         <Route path="/admin" element={<AdminPanel />} />
       </Routes>

a6fb6f2196fe4cca4a363c672c3a99acc835c25a
JulianBritos <julianbritos7@gmail.com>
2025-07-12 20:25:01 -0300

    register bug solved by adding the django allauth version un requirements.txt

--- backend/apps/users/email_config.py
+++ backend/apps/users/email_config.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 def get_email_subject_prefix():
     """Retorna el prefijo personalizado para los asuntos de email"""
-    return getattr(settings, 'ACCOUNT_EMAIL_SUBJECT_PREFIX', '[Tu Sitio Web] ')
+    return getattr(settings, 'ACCOUNT_EMAIL_SUBJECT_PREFIX', 'Cosmo play')
 
 # Configuración de asuntos personalizados
 EMAIL_SUBJECTS = {
